0e567c6504fa0485b4efe0c0967571dca1b294b2
Sebastián Silva <sebasilvac88@gmail.com>
2016-06-17 12:47:35 -0400

    creación, edición y eliminación de hitos

---
+++ sql/20160617 01 Create table Hitos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `hitos` (
+  `hito_id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `actv_id` INT NOT NULL COMMENT '',
+  `created_at` DATETIME NOT NULL COMMENT '',
+  `updated_at` DATETIME NOT NULL COMMENT '',
+  `hito_nombre` VARCHAR(45) NOT NULL COMMENT '',
+  `hito_descripcion` VARCHAR(250) NOT NULL COMMENT '',
+  PRIMARY KEY (`hito_id`)  COMMENT '');
