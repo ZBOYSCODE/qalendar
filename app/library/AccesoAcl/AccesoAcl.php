5f82d02321c1403715a09cd446066a1105aff016
Jorge Silva A <jorge.silva@zentagroup.com>
2016-06-19 14:05:26 -0400

    fix acl

--- app/library/AccesoAcl/AccesoAcl.php
+++ app/library/AccesoAcl/AccesoAcl.php
@@ -14,6 +14,8 @@
                                                     'editar'    		=> 1,
 													'crearevento'		=> 1,
 													'guardarevento'		=> 1,
+													'verperfilevento'	=> 1,
+                                                    'crearhito'			=> 1,
                                                     'uploadarchivo'		=> 1,
                                                     'downloadfile'		=> 1,
                                                     'listaarchivos'		=> 1
